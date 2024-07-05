7229515d25889caa9aec5e43c4e1e37bad6c6ac4
MANUELT3 <0321101244@ut-tijuana.edu.mx>
2024-07-05 08:57:39 -0700

    e

---
+++ Examen2/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import SignIn from './src/components/SignIn';
+import SignUp from './src/components/SignUp';
+import Home from './src/components/Home';
+import Welcome from './src/components/Welcome';
+
+const Stack = createNativeStackNavigator();
+
+const App = () => {
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Welcome">
+        <Stack.Screen name="Welcome" component={Welcome} />
+        <Stack.Screen name="SignIn" component={SignIn} />
+        <Stack.Screen name="SignUp" component={SignUp} />
+        <Stack.Screen name="Home" component={Home} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default App;
